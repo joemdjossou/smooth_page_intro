38f1d5573af86de21c6580f692822a655eb5a54a
DJOSSOU Yaovi Emmanuel Josué <joemdjossou@gmail.com>
2024-03-10 05:31:36 +0100

    Update onboarding_screen.dart

--- lib/onboarding_screen.dart
+++ lib/onboarding_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:smooth_page_intro/intro_screens/intro_page.dart';
 import 'package:smooth_page_intro/pages/home_page.dart';
@@ -20,127 +21,130 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          PageView(
-            controller: _controller,
-            onPageChanged: (index) {
-              setState(() {
-                isLastPage = (index == 3);
-              });
-            },
-            children: [
-              IntroPage(
-                lottieAsset: 'assets/lottie/connect.json',
-                text: 'Welcome to ChatBee! Get ready to connect with like-minded individuals!',
-                color: brightBlue,
-              ),
-              IntroPage(
-                lottieAsset: 'assets/lottie/girl_connect.json',
-                text: 'Let\'s kickstart your journey to meaningful connections!',
-                color: brightRed,
-              ),
-              IntroPage(
-                lottieAsset: 'assets/lottie/chatBOT.json',
-                text: 'Let\'s dive in and explore the power of connection together! Let\'s make every chat count!',
-                color: brightGreen,
-              ),
-              IntroPage(
-                lottieAsset: 'assets/lottie/allhuman.json',
-                text: 'Let\'s make magic happen, one chat at a time!!!',
-                color: brightOrange,
-              ),
-            ],
-          ),
-
-          //dot controller
-          Container(
-            alignment: const Alignment(0, 0.85),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle.dark,
+      child: Scaffold(
+        body: Stack(
+          children: [
+            PageView(
+              controller: _controller,
+              onPageChanged: (index) {
+                setState(() {
+                  isLastPage = (index == 3);
+                });
+              },
               children: [
-                //button to skip
-                GestureDetector(
-                  onTap: () {
-                    _controller.jumpToPage(3);
-                  },
-                  child: Container(
-                    padding: const EdgeInsets.all(20.0),
-                    margin: const EdgeInsets.symmetric(horizontal: 25.0),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50.0),
-                      color: Colors.white,
-                    ),
-                    child: const Text(
-                      'Skip',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
+                IntroPage(
+                  lottieAsset: 'assets/lottie/connect.json',
+                  text: 'Welcome to ChatBee! Get ready to connect with like-minded individuals!',
+                  color: brightBlue,
+                ),
+                IntroPage(
+                  lottieAsset: 'assets/lottie/girl_connect.json',
+                  text: 'Let\'s kickstart your journey to meaningful connections!',
+                  color: brightRed,
+                ),
+                IntroPage(
+                  lottieAsset: 'assets/lottie/chatBOT.json',
+                  text: 'Let\'s dive in and explore the power of connection together! Let\'s make every chat count!',
+                  color: brightGreen,
+                ),
+                IntroPage(
+                  lottieAsset: 'assets/lottie/allhuman.json',
+                  text: 'Let\'s make magic happen, one chat at a time!!!',
+                  color: brightOrange,
+                ),
+              ],
+            ),
+
+            //dot controller
+            Container(
+              alignment: const Alignment(0, 0.85),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  //button to skip
+                  GestureDetector(
+                    onTap: () {
+                      _controller.jumpToPage(3);
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.all(20.0),
+                      margin: const EdgeInsets.symmetric(horizontal: 25.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50.0),
+                        color: Colors.white,
+                      ),
+                      child: const Text(
+                        'Skip',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
                   ),
-                ),
 
-                //dot indicator
-                SmoothPageIndicator(
-                  controller: _controller,
-                  count: 4,
-                ),
+                  //dot indicator
+                  SmoothPageIndicator(
+                    controller: _controller,
+                    count: 4,
+                  ),
 
-                //next or done
-                isLastPage
-                    ? GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const HomePage(),
+                  //next or done
+                  isLastPage
+                      ? GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const HomePage(),
+                              ),
+                            );
+                          },
+                          child: Container(
+                            padding: const EdgeInsets.all(20.0),
+                            margin: const EdgeInsets.symmetric(horizontal: 25.0),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50.0),
+                              color: Colors.white,
                             ),
-                          );
-                        },
-                        child: Container(
-                          padding: const EdgeInsets.all(20.0),
-                          margin: const EdgeInsets.symmetric(horizontal: 25.0),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(50.0),
-                            color: Colors.white,
+                            child: const Text(
+                              'Done',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
                           ),
-                          child: const Text(
-                            'Done',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
+                        )
+                      : GestureDetector(
+                          onTap: () {
+                            _controller.nextPage(
+                              duration: const Duration(
+                                milliseconds: 500,
+                              ),
+                              curve: Curves.easeIn,
+                            );
+                          },
+                          child: Container(
+                            padding: const EdgeInsets.all(20.0),
+                            margin: const EdgeInsets.symmetric(horizontal: 25.0),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50.0),
+                              color: Colors.white,
+                            ),
+                            child: const Text(
+                              'Next',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
                           ),
                         ),
-                      )
-                    : GestureDetector(
-                        onTap: () {
-                          _controller.nextPage(
-                            duration: const Duration(
-                              milliseconds: 500,
-                            ),
-                            curve: Curves.easeIn,
-                          );
-                        },
-                        child: Container(
-                          padding: const EdgeInsets.all(20.0),
-                          margin: const EdgeInsets.symmetric(horizontal: 25.0),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(50.0),
-                            color: Colors.white,
-                          ),
-                          child: const Text(
-                            'Next',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
